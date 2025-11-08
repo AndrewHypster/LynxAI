d72e8326d4c0e402c0230725c5adf4610539661c
AndrewHypster <andrewgrechukh123@gmail.com>
2025-11-08 14:06:53 +0200

    first

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,13 +1,28 @@
-'use client'
-import { GoogleButton } from "@/components/GoogleButton";
-import { SignInForm } from "../../components/sign-in-form";
+"use client";
 
-export default async function Signin() {
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
+import { GoogleButton } from "@/components/GoogleButton";
+import { SignInForm } from "@/components/sign-in-form";
+
+function SignInContent() {
+  const searchParams = useSearchParams();
+  const ref = searchParams.get("ref"); // наприклад ?ref=telegram
+
   return (
     <div>
       <h1>Sign In</h1>
+      {ref && <p>Redirect from: {ref}</p>}
       <GoogleButton />
       <SignInForm />
     </div>
+  );
+}
+
+export default function Signin() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignInContent />
+    </Suspense>
   );
 }
